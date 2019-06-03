88b3d573f5fcf82b65d49059a279b53b0cc150bb
Michael Hughes <mhughes36@live.co.uk>
2019-06-03 09:30:19 +0100

    First commit

---
+++ spec/thermostatSpec.js
@@ -0,0 +1,14 @@
+'use strict';
+
+describe('Thermostat', function() {
+
+  var thermostat;
+
+  beforeEach(function() {
+    thermostat = new Thermostat();
+  });
+
+  it('starts at 20 degrees', function() {
+    expect(thermostat.temperature).toEqual(20);
+  });
+});
---
+++ src/thermostat.js
@@ -0,0 +1,5 @@
+'use strict';
+
+function Thermostat() {
+  this.temperature = 20;
+}
