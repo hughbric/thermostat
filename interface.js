47d3509ca88c8256dca680bc92bd517ad60d3c60
Michael Hughes <mhughes36@live.co.uk>
2019-06-04 12:36:01 +0100

    jQuery interface

---
+++ interface.js
@@ -0,0 +1,35 @@
+$(document).ready(function() {
+  var thermostat = new Thermostat();
+  updateTemperature();
+
+  $('#temp-up').click(function() {
+    thermostat.up();
+    updateTemperature();
+  });
+
+  $('#temp-down').click(function() {
+    thermostat.down();
+    updateTemperature();
+  });
+
+  $('#temp-reset').click(function() {
+    thermostat.resetTemperature();
+    updateTemperature();
+  });
+
+  $('#psm-on').click(function() {
+    thermostat.switchPowerSavingModeOn();
+    $('#power-saving').text('on');
+    updateTemperature();
+  });
+
+  $('#psm-off').click(function() {
+    thermostat.switchPowerSavingModeOff();
+    $('#power-saving').text('off');
+    updateTemperature();
+  });
+
+  function updateTemperature() {
+    $('#temperature').text(thermostat.temperature);
+  }
+});
